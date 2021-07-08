6103048663399ab76d1c3b61e4f4fa4698b2dd60
Naresh <pnareshsatya@gmail.com>
2021-07-08 13:27:11 +0530

    Uc3

--- TestAddressBook.java
+++ TestAddressBook.java
@@ -1,14 +1,28 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.Scanner;
+
 public class TestAddressBook {
+	@SuppressWarnings("resource")
 	public static void main(String args[]) {
 		Contact contact1 = new Contact1();
-
+		
 		System.out.println("Welcome to Address Book");
-
+		
 		AddressBook addressBook = new AddressBook();
-
-		addressBook.addContact(contact1);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1.Add 2.Update 3.Delete");
+		int option = sc.nextInt();
+		switch(option) {
+			case 1: addressBook.add(contact1);
+					break;
+			case 2: addressBook.update(contact1);
+					break;
+			default: System.out.println("Enter valid option: ");
+		}		
+		//addressBook.add(contact1);
+		//addressBook.update(contact1);
 		addressBook.print();
+		
 	}
 }
