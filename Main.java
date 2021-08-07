d2e10e9a20763118d46cb6d6c2679f61927e1c1a
NareshPusarla <86452410+NareshPusarla@users.noreply.github.com>
2021-08-07 09:56:42 +0530

    Uc13

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
 package com.bridgelbaz.addressbook;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
 public class Main {
-	
 
 	public static void main(String[] args) {
 //		Logger logger = org.apache.log4j.LogManager.getLogger(Main.class);
+
 		System.out.println("Welcome to Address Book");
 
 		AddressBook addressBook = new AddressBook();
@@ -55,6 +61,57 @@ public class Main {
 			default:
 				System.out.println("Enter valid option: ");
 			}
+		}
+		
+		try {
+			// Created directory named AddressBook
+			Path p = Paths.get("C:\\Users\\Naresh\\Documents\\AddressBook");
+			if (Files.exists(p)) {
+				System.out.println("Directory already existed.");
+			} else {
+				Path donePath = Files.createDirectory(p);
+				System.out.println("Directory Created at " + donePath.toString());
+			}
+
+			// Creating a text file named AddreeBook in the directory
+			Path file = Paths.get("C:\\Users\\Naresh\\Documents\\AddressBook\\AddressBook.txt");
+			if (Files.exists(file)) {
+				System.out.println("File already existed.");
+			} else {
+				Path doneFile = Files.createFile(file);
+				System.out.println("File created at " + doneFile.toString());
+			}
+
+			// Reading all the lines in the text file addressbook
+			List<String> data = Files.readAllLines(file);
+			for (String s : data) {
+				System.out.println(s);
+			}
+
+			// Writing the string content into the file.
+			String content = "A love story is created in 1st file are created";
+			Path file1 = Paths.get("C:\\Users\\Naresh\\Documents\\AddressBook\\AddressBook1.txt");
+			if (Files.exists(file1)) {
+				System.out.println("File already exists");
+			} else {
+				Files.write(file1, content.getBytes());
+
+				System.out.println();
+				System.out.println("String content is added to the file.");
+
+			}
+
+			/*
+			 * // Copying data from file to file2 Path p1 =
+			 * Paths.get("C:\\Users\\Naresh\\Documents\\AddressBook\\AddressBookCopy.txt");
+			 * if (Files.exists(p1)) { System.out.println("Already existed"); } else { Path
+			 * doneFile1 = Files.createFile(p1);
+			 * 
+			 * Files.write(doneFile1, data); System.out.println(); }
+			 */
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
